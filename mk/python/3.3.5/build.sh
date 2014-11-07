96acee1510c22c243ed4e9eff889c5b6cd7dbcec
Thomas Perl <m@thp.io>
2014-11-07 14:27:58 +0100

    Build some modules statically

--- mk/python/3.3.5/build.sh
+++ mk/python/3.3.5/build.sh
@@ -28,6 +28,7 @@ patch -Ep1 < "${FILESDIR}/${PACKAGE}-android-libmpdec.patch" || exit 1
 patch -p1  < "${FILESDIR}/${PACKAGE}-android-misc.patch" || exit 1
 patch -p1  < "${FILESDIR}/${PACKAGE}-fdatasync.patch" || exit 1
 
+cp "${FILESDIR}/Setup" Modules/Setup || exit 1
 ./configure CROSS_COMPILE_TARGET=yes HOSTPYTHON="$(pwd)/hostpython" CONFIG_SITE=config.site --prefix="${PREFIX}" --host="${TARGET}" --build="${HOST}" --disable-ipv6 || exit 1
 make CROSS_COMPILE_TARGET=yes HOSTPYTHON="$(pwd)/hostpython" HOSTPGEN="$(pwd)/Parser/hostpgen" || exit 1
 make CROSS_COMPILE_TARGET=yes HOSTPYTHON="$(pwd)/hostpython" HOSTPGEN="$(pwd)/Parser/hostpgen" install || exit 1
